e08cb16d2a2b2faa0488b313df367fdae33337c6
Tynarus <github@tyn.dev>
2020-12-24 13:49:21 -0600

    Adding missing export

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ export * from './filestore/filestore';
 export * from './filestore/file-data';
 export * from './filestore/archive';
 export * from './filestore/file-index';
+
 export * from './filestore/stores/binary-store';
 export * from './filestore/stores/midi-store';
 export * from './filestore/stores/ogg-store';
 export * from './filestore/stores/sound-store';
 export * from './filestore/stores/sprite-store';
+export * from './filestore/stores/region-store';
+
 export * from './filestore/data/filestore-loader';
 export * from './filestore/data/compression';
 export * from './filestore/data/chunk';

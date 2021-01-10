25a4169e89083886ca70bdbe904810f3751a5066
Tynarus <github@tyn.dev>
2021-01-10 13:43:58 -0600

    Pushing v0.6.0 version number

--- src/run.ts
+++ src/run.ts
@@ -2,10 +2,3 @@ import { Filestore } from './filestore/filestore';
 
 
 const filestore = new Filestore('./packed', './config');
-
-const font = filestore.fontStore.fonts.q8_full;
-
-console.log(font.drawString('hello world!', 0x244BDC));
-
-//filestore.spriteStore.getSpritePack('mapback').decode().sprites[0].toBase64().then(console.log);
-

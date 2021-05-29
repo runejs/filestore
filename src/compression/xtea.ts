c335fa9cb1c0f6636798906a6904b1097413f296
Tynarus <github@tyn.dev>
2021-05-29 17:10:19 -0500

    Writing untested xtea encryption code, but that's all for tonight ;)

--- src/compression/xtea.ts
+++ src/compression/xtea.ts
@@ -10,9 +10,30 @@ export default class Xtea {
         return keys?.length === 4 && (keys[0] !== 0 || keys[1] !== 0 || keys[2] !== 0 || keys[3] !== 0);
     }
 
-    // @TODO stubbed
+    // @TODO untested
     public static encrypt(input: ByteBuffer, keys: number[], length: number): ByteBuffer {
-        return null;
+        const encryptedBuffer = new ByteBuffer(length);
+        const chunks = length / 8;
+        input.readerIndex = 0;
+
+        for(let i = 0; i < chunks; i++) {
+            let v0 = input.get('int');
+            let v1 = input.get('int');
+            let sum = 0;
+            let delta = -0x61c88647;
+
+            let rounds = 32;
+            while(rounds-- > 0) {
+                v0 += ((sum + keys[sum & 3]) ^ (v1 + ((v1 >>> 5) ^ (v1 << 4))));
+                sum += delta
+                v1 += ((v0 + ((v0 >>> 5) ^ (v0 << 4))) ^ (keys[(sum >>> 11) & 3] + sum));
+            }
+
+            encryptedBuffer.put(v0, 'int');
+            encryptedBuffer.put(v1, 'int');
+        }
+
+        return encryptedBuffer.flipWriter();
     }
 
     public static decrypt(input: ByteBuffer, keys: number[], length: number): ByteBuffer {

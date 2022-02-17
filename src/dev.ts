42b21af48b176fa72685c92c2d008214703455c8
Kikorono <kikorono.vr@gmail.com>
2022-02-16 21:03:08 -0600

    rc.0 update

--- src/dev.ts
+++ src/dev.ts
@@ -2,6 +2,5 @@ import { Store } from './fs';
 import { join } from 'path';
 
 
-const store = new Store(435, join('..', 'store'), join('..', 'store', 'output'));
+const store = Store.create(435, join('..', 'store'), join('..', 'store', 'output'));
 
-console.log(store.hashFileName('cabbage'));
